ed561f112d935ff45a8cc178427d9b16315178d5
Hao <haodu.a@gmail.com>
2026-02-27 17:09:03 +0800

    .

--- src/simple_gate_client.rs
+++ src/simple_gate_client.rs
@@ -1,19 +1,19 @@
-use std::collections::HashMap;
-use hex;
-use reqwest;
-use serde_json;
 use chrono::Utc;
-use hmac::{Hmac, Mac};
-use sha2::Sha512;
-use serde_json::Value;
-use reqwest::Method;
-use url::Url;
+use hex;
 use hmac::digest::Digest;
+use hmac::{Hmac, Mac};
+use reqwest;
+use reqwest::Method;
+use serde_json;
+use serde_json::Value;
+use sha2::Sha512;
+use std::collections::HashMap;
+use url::Url;
 
 pub struct SimpleGateClient {
     api_key: String,
     api_secret: String,
-    proxy: Option<String>,  // Format: "host:port"
+    proxy: Option<String>, // Format: "host:port"
 }
 
 impl SimpleGateClient {
@@ -26,8 +26,14 @@ impl SimpleGateClient {
     }
 
     // module: spot/time
-    pub async fn send_request(&self, method: Method, module: &str, params: &HashMap<String, String>) -> Result<Value, Box<dyn std::error::Error>> {
-        let query_string = params.iter()
+    pub async fn send_request(
+        &self,
+        method: Method,
+        module: &str,
+        params: &HashMap<String, String>,
+    ) -> Result<Value, Box<dyn std::error::Error>> {
+        let query_string = params
+            .iter()
             .map(|(key, value)| format!("{}={}", key, value))
             .collect::<Vec<String>>()
             .join("&");
@@ -64,29 +70,30 @@ impl SimpleGateClient {
         let mut client_builder = reqwest::Client::builder();
         if let Some(proxy_addr) = &self.proxy {
             let proxy_url = format!("http://{}", proxy_addr);
-            let proxy = reqwest::Proxy::https(&proxy_url)
-                .map_err(|e| Box::new(std::io::Error::new(std::io::ErrorKind::Other, format!("Failed to create proxy: {}", e))))?;
+            let proxy = reqwest::Proxy::https(&proxy_url).map_err(|e| {
+                Box::new(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    format!("Failed to create proxy: {}", e),
+                ))
+            })?;
             client_builder = client_builder.proxy(proxy);
         }
-        let client = client_builder.build()
-            .map_err(|e| Box::new(std::io::Error::new(std::io::ErrorKind::Other, format!("Failed to build client: {}", e))))?;
+        let client = client_builder.build().map_err(|e| {
+            Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("Failed to build client: {}", e),
+            ))
+        })?;
 
         // Send request
         let response = match method {
-            Method::GET => {
-                client.get(full_url)
-                    .headers(headers)
-                    .send()
-                    .await?
-            },
-            Method::POST => {
-                client.post(&url_string)
-                    .headers(headers)
-                    .send()
-                    .await?
-            },
+            Method::GET => client.get(full_url).headers(headers).send().await?,
+            Method::POST => client.post(&url_string).headers(headers).send().await?,
             _ => {
-                return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, format!("Unsupported method: {:?}", method))));
+                return Err(Box::new(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    format!("Unsupported method: {:?}", method),
+                )));
             }
         };
 
@@ -96,16 +103,28 @@ impl SimpleGateClient {
 
             return Ok(response_json.clone());
         } else {
-            return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, format!("Response: status = [{}]", response.status()))));
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("Response: status = [{}]", response.status()),
+            )));
         }
     }
 
-    fn sign_hmac(&self, method: &Method, path: &str, query_string: &str, payload: &str, timestamp: &str, key: &str) -> Result<String, Box<dyn std::error::Error>> {
+    fn sign_hmac(
+        &self,
+        method: &Method,
+        path: &str,
+        query_string: &str,
+        payload: &str,
+        timestamp: &str,
+        key: &str,
+    ) -> Result<String, Box<dyn std::error::Error>> {
         let hashed_payload = hex::encode(Sha512::digest(payload.as_bytes()).to_vec());
 
         let s = format!(
             "{}\n{}\n{}\n{}\n{}",
-            method, path, query_string, hashed_payload, timestamp);
+            method, path, query_string, hashed_payload, timestamp
+        );
 
         let mut mac = Hmac::<Sha512>::new_from_slice(key.to_string().as_bytes())?;
         mac.update(s.as_bytes());
@@ -123,20 +142,34 @@ mod tests {
     #[tokio::test]
     async fn test_request_without_signature() {
         let client = SimpleGateClient::new("", "", &None);
-        let result = client.send_request(Method::GET, "spot/time", &HashMap::new()).await;
+        let result = client
+            .send_request(Method::GET, "spot/time", &HashMap::new())
+            .await;
 
         let now_ms = Utc::now().timestamp_millis();
-        let server_time_ms = result.unwrap().get("server_time").unwrap().as_i64().unwrap();
+        let server_time_ms = result
+            .unwrap()
+            .get("server_time")
+            .unwrap()
+            .as_i64()
+            .unwrap();
         assert!((server_time_ms - now_ms).abs() < 2000);
     }
 
     #[tokio::test]
     async fn test_request_without_signature_with_proxy() {
         let client = SimpleGateClient::new("", "", &Some("oas.w4iq.com:3128".to_string()));
-        let result = client.send_request(Method::GET, "spot/time", &HashMap::new()).await;
+        let result = client
+            .send_request(Method::GET, "spot/time", &HashMap::new())
+            .await;
 
         let now_ms = Utc::now().timestamp_millis();
-        let server_time_ms = result.unwrap().get("server_time").unwrap().as_i64().unwrap();
+        let server_time_ms = result
+            .unwrap()
+            .get("server_time")
+            .unwrap()
+            .as_i64()
+            .unwrap();
         assert!((server_time_ms - now_ms).abs() < 2000);
     }
 }
